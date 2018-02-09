6970296b55538e5be6b234ecb2e0c49adc9dcd91
pbokey <pbokey@gmail.com>
2018-02-09 00:54:04 -0500

    changed some things back

--- esketit_bot.py
+++ esketit_bot.py
@@ -8,7 +8,7 @@ access_token_secret = 'oNfrJORH0dYw92il8aJ2QDGGnVCNuJaVWXAHVAgy6DJON'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth
+api = tweepy.API(auth)
                  
 if __name__ == '__main__':
     while True:
