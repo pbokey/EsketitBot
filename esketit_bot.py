0c0d9b7934712ba29618fbf7bdf9adbe8f9537b6
pbokey <pbokey@gmail.com>
2018-02-09 02:50:42 -0500

    fixed loop count

--- esketit_bot.py
+++ esketit_bot.py
@@ -5,10 +5,9 @@ from credentials import *
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-                 
+t_count = 4
 if __name__ == '__main__':
     while True:
-        t_count = 3
         api.update_status("ESKETI{0} #lilpump".format('T' * t_count))
         t_count += 1
         sleep(3600)
