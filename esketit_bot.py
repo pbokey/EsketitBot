5c2d5a0233bb87e7d1dbc2de5e344e0e1242c849
pbokey <pbokey@gmail.com>
2018-02-09 00:48:39 -0500

    testing if heroku realized compile errors

--- esketit_bot.py
+++ esketit_bot.py
@@ -8,11 +8,11 @@ access_token_secret = 'oNfrJORH0dYw92il8aJ2QDGGnVCNuJaVWXAHVAgy6DJON'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-
-while True:
-    t_count = 3
-    api.update_status("ESKETI{0} #lilpump".format('T' * t_count))
-    t_count += 1
-    sleep(3600)
+api = tweepy.API(auth
+                 
+if __name__ == '__main__':
+    while True:
+        t_count = 3
+        api.update_status("ESKETI{0} #lilpump".format('T' * t_count))
+        t_count += 1
+        sleep(3600)
