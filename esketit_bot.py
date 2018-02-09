0cbe79b6d5ab6a39d305f57cd4d5859b76c88f01
pbokey <pbokey@gmail.com>
2018-02-09 01:00:55 -0500

    added gitignore

--- esketit_bot.py
+++ esketit_bot.py
@@ -1,10 +1,6 @@
 import tweepy
 from time import sleep
-
-consumer_key = 'rihI6V6y8dMviBKwp38zaAfhb'
-consumer_secret = 'e0P2dCEpFwbRH86BlnjSuPULNpwtMLkNVzUia61dUiH4elgMGb'
-access_token = '961825486199406592-kKGL6eqVK4knN6BciPbCkqXPfCPqipk'
-access_token_secret = 'oNfrJORH0dYw92il8aJ2QDGGnVCNuJaVWXAHVAgy6DJON'
+from credentials import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
